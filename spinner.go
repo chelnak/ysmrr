7e55a805f76dbb96a46b877100b9b613123d211e
Craig Gumbley <craiggumbley@gmail.com>
2022-07-17 15:59:24 +0100

    Add mutext to update operations

--- spinner.go
+++ spinner.go
@@ -1,12 +1,15 @@
 package ysmrr
 
 import (
+	"sync"
+
 	"github.com/chelnak/ysmrr/pkg/colors"
 	"github.com/fatih/color"
 )
 
 // Spinner manages a single spinner
 type spinner struct {
+	mutex         sync.Mutex
 	spinnerColor  *color.Color
 	completeColor *color.Color
 	errorColor    *color.Color
@@ -17,16 +20,25 @@ type spinner struct {
 
 // Update updates the spinner message
 func (s *spinner) Update(msg string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	s.msg = msg
 }
 
 // Complete marks the spinner as complete
 func (s *spinner) Complete() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	s.complete = true
 }
 
 // Error marks the spinner as error
 func (s *spinner) Error() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
 	s.err = true
 }
 
